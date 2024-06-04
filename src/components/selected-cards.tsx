900b68ff51aa4d2c7f5cc6ce99eb6ddbfc5e47ae
Felipe <felipe.calderon321@gmail.com>
2024-06-04 17:22:45 -0400

    instalando react icons

--- src/components/selected-cards.tsx
+++ src/components/selected-cards.tsx
@@ -1,18 +1,19 @@
 "use client"
 import useCardStore from "@/stores/cardStore"
-import { Card, CardHeader, Image } from "@nextui-org/react"
+import { Button, Card, CardHeader, Image } from "@nextui-org/react"
 import { cards } from "@/lib/arrCards"
 import { userStore } from "@/stores/userStore"
-export default function SelectedCards() {
-    const { flippedCards } = useCardStore()
-    const { name, born, question } = userStore()
+import { MdOutlineDoubleArrow } from "react-icons/md"
 
+export default function SelectedCards() {
+    const { flippedCards, limit } = useCardStore()
+    // const { name, born, question } = userStore()
+    const completed = flippedCards.size === limit
     const selectedCards = cards.filter((card) => flippedCards.has(card.nombre))
 
-    console.log({ name, born, question })
     if (selectedCards.length > 0)
         return (
-            <div>
+            <div className="pb-6">
                 <div className="text-center text-white">
                     <h2 className="text-2xl mb-3">Cartas escogidas</h2>
                 </div>
@@ -30,11 +31,19 @@ export default function SelectedCards() {
                                 <div className="flex flex-col">
                                     <p className="text-small text-default-500">Tipo: {card.categoria}</p>
                                     <p className="text-md">{card.nombre}</p>
+                                    <p className="text-xs italic">{card.representa.map((rep) => rep + " ")}</p>
                                 </div>
                             </CardHeader>
                         </Card>
                     ))}
                 </div>
+                {completed && (
+                    <div className="fixed bottom-6 right-6">
+                        <Button color="secondary" size="lg">
+                            Analizar la tirada <MdOutlineDoubleArrow />
+                        </Button>
+                    </div>
+                )}
             </div>
         )
 }
