050d856265a89e1d02a6890c21ae92015028e749
Felipe <felipe.calderon321@gmail.com>
2024-07-05 13:15:49 -0400

    useMemo para filtrar tabla

--- src/components/tables/consultas-tabla.tsx
+++ src/components/tables/consultas-tabla.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Carta } from "@/lib/interfaces"
-import { Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react"
-import React, { useMemo, useState } from "react"
+import { Input, Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react"
+import React, { ChangeEvent, useEffect, useMemo, useState } from "react"
 import ModalTarot from "../modal"
 import { calcularEdad } from "@/lib/calcularEdad"
 import { fixNombre } from "@/lib/fixNombre"
@@ -19,25 +19,43 @@ const Consultastabla = ({ consultas }: { consultas: Consulta[] }) => {
     const [page, setPage] = useState(1)
     const [openModal, setOpen] = useState(false)
     const [consulta, setConsulta] = useState("")
+    const [items, setItems] = useState<Consulta[]>([])
+    const [searchTerm, setSearchTerm] = useState("")
     const rowsPerPage = 15
 
-    const pages = Math.ceil(consultas.length / rowsPerPage)
+    const pages = useMemo(() => Math.ceil(consultas.length / rowsPerPage), [consultas, rowsPerPage])
 
     const verConsulta = (consulta: string) => {
         setOpen(true)
         setConsulta(consulta)
     }
 
-    const items = useMemo(() => {
+    const normalizeString = (str: string) => {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    }
+
+    const normalizedSearchTerm = useMemo(() => normalizeString(searchTerm.toLowerCase()), [searchTerm])
+
+    const consultasFiltradas = useMemo(
+        () => consultas.filter((c) => normalizeString(c.name.toLowerCase()).includes(normalizedSearchTerm)),
+        [consultas, normalizedSearchTerm]
+    )
+
+    useEffect(() => {
         const start = (page - 1) * rowsPerPage
         const end = start + rowsPerPage
-
-        return consultas.slice(start, end)
-    }, [page, consultas])
-
+        setItems(consultasFiltradas.slice(start, end))
+    }, [page, consultasFiltradas])
     return (
         <>
             <Paginacion page={page} pages={pages} setPage={setPage} />
+            <Input
+                color="secondary"
+                name="nombre"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-fit mx-auto"
+                label="Nombre del consultante"
+            />
             <Table isStriped aria-label="Tabla de consultas de usuarios publica" selectionMode="single" color="secondary">
                 <TableHeader>
                     <TableColumn>NOMBRE</TableColumn>
@@ -48,7 +66,9 @@ const Consultastabla = ({ consultas }: { consultas: Consulta[] }) => {
                     {items.map((consulta) => (
                         <TableRow key={consulta.id} className="cursor-pointer" onClick={() => verConsulta(consulta.answer)}>
                             <TableCell className="w-1/5">{fixNombre(consulta.name)}</TableCell>
-                            <TableCell className="w-1/12">{calcularEdad(consulta.born)} años</TableCell>
+                            <TableCell className="w-1/12">
+                                {calcularEdad(consulta.born)} años <span className="text-xs">({consulta.born})</span>
+                            </TableCell>
                             <TableCell className="whitespace-nowrap overflow-hidden overflow-ellipsis max-w-xs md:max-w-md">
                                 {consulta.question}
                             </TableCell>
