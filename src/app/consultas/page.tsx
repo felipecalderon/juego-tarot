eae55341adfa0fb4d7f7027f14314e5be7286a4f
Felipe <felipe.calderon321@gmail.com>
2024-08-12 21:35:36 -0400

    oc automatica mueve stock

--- src/app/consultas/page.tsx
+++ src/app/consultas/page.tsx
@@ -8,12 +8,13 @@ export default async function ConsultasPage() {
         cache: "no-store",
     })
     const consultas = await fetchdata.json()
-    return (
-        <section className="min-h-screen px-6 py-3 md:px-24 md:py-10 flex flex-col gap-6">
-            <h1 className="text-3xl md:text-6xl text-yellow-300 font-bold text-center text-nowrap">Base de consultas pública</h1>
-            <h2 className="text-2xl md:text-3xl text-white text-center text-nowrap">Transparencia de la información</h2>
-            <ConsultasTop consultas={consultas} />
-            <Consultastabla consultas={consultas} />
-        </section>
-    )
+    if (consultas && consultas.length > 0)
+        return (
+            <section className="min-h-screen px-6 py-3 md:px-24 md:py-10 flex flex-col gap-6">
+                <h1 className="text-3xl md:text-6xl text-yellow-300 font-bold text-center text-nowrap">Base de consultas pública</h1>
+                <h2 className="text-2xl md:text-3xl text-white text-center text-nowrap">Transparencia de la información</h2>
+                <ConsultasTop consultas={consultas} />
+                <Consultastabla consultas={consultas} />
+            </section>
+        )
 }
